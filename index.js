394086c0fbfe768444f6f412c231afb26deafe83
danielbeyene <daniel.beyene@student.moringashcool.com>
2024-04-03 14:03:13 +0300

    Creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// Select the 'main' element with the id 'main'
+const mainElement = document.querySelector('main#main');
+
+// If the element exists, remove it from the DOM
+if (mainElement) {
+  mainElement.remove();
+}
+
+const newHeader = document.querySelector('h1');
